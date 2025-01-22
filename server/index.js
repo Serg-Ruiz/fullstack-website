d6940bfa0cdeb329d778862ae65992a6cc6c10ae
Serg-Ruiz <sergioruiz@fullstackprogrammer.net>
2025-01-21 19:11:44 -0600

    setup post and get request for user info

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const db = require("./models");
+
+// Routers
+const userRouter = require("./routes/users");
+app.use("/users", userRouter);
 
 db.sequelize.sync().then(() => 
 {
---
+++ server/routes/users.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const { Users } = require("../models");  // Use `Users` (with uppercase 'U')
+
+router.get('/', (req, res) => {
+    res.send("hello world");
+});
+
+router.post("/", async (req, res) => 
+{
+    const { username, country, score } = req.body;
+
+    // Validate incoming data
+    if (!username || !country || !score) 
+    {
+        return res.status(400).json({ error: "Username, country, and score are required" });
+    }
+
+    try 
+    {
+        // Assuming you have a "Users" model, create a new user
+        const user = await Users.create
+        ({
+            username,
+            country,
+            score
+        });
+
+    // Respond with the created user    
+    res.status(201).json(user);  // Respond with the correct variable `user`
+    } 
+    catch (error) 
+    {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred while creating the user" });
+    }
+});
+
+module.exports = router;
